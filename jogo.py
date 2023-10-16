92e96c5aef3f4f724f5984dd657f436cb0bb892a
rafa-ken <rafaelkenmiyamoto@gmail.com>
2023-10-16 16:52:17 -0300

    Check 2

--- jogo.py
+++ jogo.py
@@ -1,48 +1,65 @@
 # TODO: Escreva seu código aqui (fique à vontade para adicionar outros arquivos, mas use este como ponto de entrada)
 import pygame
+import random
 #inicializa
 def inicializa():
     pygame.init()
 
     #janela
-    window = pygame.display.set_mode((320, 240))
+    window = pygame.display.set_mode((400, 600))
     pygame.display.set_caption('Jogo do Rafak')
 
-    #retorno
-    return window
+    #dicionario assets
+    assets = {}
+    assets['nave'] = pygame.image.load('assets/img/playerShip1_orange.png')
+    assets['fundo'] = pygame.image.load('assets/img/starfield.png')
+
+    return window, assets
 
 def recebe_eventos():
-    #Inicia estruturas de dados
+    #inicia estruturas de dados
     game = True
 
-    # Loop principal
+    # loop principal
     while game:
-        #Trata eventos
+        #trata eventos
         for event in pygame.event.get():
-            #Verifica consequências
+            #verifica consequências
             if event.type == pygame.QUIT:
                 game = False
         return game
 
-def desenha(window):
-    #Gera saídas
-    window.fill((0,0,0))  #Preenche com a cor vermelha
+def desenha(window, assets, estrelas):
+    # gera saídas
+    window.fill((0, 0, 0))
+    fundo = pygame.transform.scale(assets['fundo'], (400, 600))
+    nave = pygame.transform.scale(assets['nave'], (69, 38))
+    window.blit(fundo, (0, 0))
+    window.blit(nave, (162, 530))
 
-    #Atualiza estado do jogo
-    pygame.display.update()  #Mostra o novo frame para o jogador
+    # desenha estrelas
+    for x, y in estrelas:
+        pygame.draw.circle(window, (255, 255, 255), (x, y), 1, 5)
+
+    # atualiza estado do jogo
+    pygame.display.update()  
     return window
 
-def game_loop(window):
+def gera_estrelas():
+    estrelas = [(random.randint(0, 400), random.randint(0, 600)) for _ in range(40)]
+    return estrelas
+
+def game_loop(window, assets):
     game = True
-    #Loop principal
+    estrelas = gera_estrelas() 
+    # loop principal
     while game:
-        #Trata eventos
+        # trata eventos
         game = recebe_eventos()
+        # gera saídas
+        desenha(window, assets, estrelas)
         if game == False:
-            return game
+            return False
 
-        #gera saidas
-        desenha(window)
-janela = inicializa()
-loop = game_loop(janela)
-
+janela, assets = inicializa()
+loop = game_loop(janela, assets)
